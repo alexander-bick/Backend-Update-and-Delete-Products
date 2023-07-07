621344bc130785293a614abc048fd31ee8e30159
Alexander Bick <AlexanderBick@me.com>
2023-07-07 12:11:13 +0200

    nearly working

--- components/Product/index.js
+++ components/Product/index.js
@@ -2,13 +2,42 @@ import useSWR from "swr";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
-import Comments from "../../../../backend-create/products/components/Comments";
+import Comments from "../Comments/index.js";
+import ProductForm from "../ProductForm";
+import { useState } from "react";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/jokes/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDelete() {
+    await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    router.push("/");
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -26,6 +55,24 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <button 
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+        >
+          <span role="img" aria-label="A pencil">
+            ✏️
+          </span>
+      </button>
+      <button onClick={handleDelete} disabled={isEditMode}>
+          <span role="img" aria-label="A cross indicating deletion">
+            ❌
+          </span>
+        </button>
+      {isEditMode && (
+        <ProductForm onSubmit={handleEditProduct} value={data} isEditMode={true} />
+      )}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
